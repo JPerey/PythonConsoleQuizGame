06e1486789055455fd81ecab695bf8e1a4ff381a
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-09-23 17:04:51 -0700

    DONE: complete for now, will create new quiz with visuals later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 import data
+from quiz_brain import QuizBrain
 
 # global variables
 question_bank = []
@@ -8,4 +9,10 @@ for q in data.question_data:
     new_q = Question(q["text"], q["answer"])
     question_bank.append(new_q)
 
-print(question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"your done! total score: {quiz.score}/12")
+
---
+++ quiz_brain.py
@@ -0,0 +1,26 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_questions(self):
+        question_amount = len(self.question_list)
+        if question_amount > self.question_number:
+            return True
+        else:
+            return False
+
+    def next_question(self):
+        current_number = self.question_number + 1
+        player_answer = input(f"Q{current_number}: {self.question_list[self.question_number].text}. True/False?: ")
+        self.check_answer(player_answer, self.question_list[self.question_number].answer, current_number)
+        self.question_number += 1
+
+    def check_answer(self, player_answer, question_answer, current_number):
+        if player_answer == question_answer:
+            self.score += 1
+            print(f"correct! current score: {self.score}/{current_number}")
+        else:
+            print(f"incorrect! current score: {self.score}/{current_number}")
+
